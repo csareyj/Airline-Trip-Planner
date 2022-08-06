be45cd01ffb5b2cbecc3c0d243a0dbae38396942
Curtis <csareyj@gmail.com>
2022-08-06 11:27:53 -0700

    Adding Mongo URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Airline-Trip-Planner', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Airline-Trip-Planner', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
