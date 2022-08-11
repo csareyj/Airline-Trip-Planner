200e39d08917d7624b3040d02f8e00354576e317
Curtis <csareyj@gmail.com>
2022-08-10 17:59:37 -0700

    User Password Hashing

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,7 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 7;
+
 const userSchema = new Schema({
     name: {
         type: String,
@@ -15,9 +18,39 @@ const userSchema = new Schema({
         required: true
     }
 });
-userSchema.pre('save', (next, ...args) => {
-    console.log(args);
-    next();
+
+userSchema.pre('save', function(next) {
+    var user = this;
+    console.log({hookThis: user});
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
 });
+
+// userSchema.pre('save', (next, ...args) => {
+//     console.log(args);
+//     next();
+// });
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
 const User = model('User', userSchema);
 module.exports = User;
