3672b1d13fca9d5efea15053cf9940e57d986744
DaveDw <93271098+DaveDw@users.noreply.github.com>
2022-08-17 21:38:42 -0400

    edits to login js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,9 @@
 import { useQuery, useMutation } from "@apollo/client";
 import { useParams, Link } from "react-router-dom";
-import { CREATE_VOTE } from "../utils/mutations";
+import { CREATE_DEPARTURE } from "../utils/mutations";
+import { CREATE_DESTINATION } from "../utils/mutations";
+import { CREATE_DURATION } from "../utils/mutations";
+import { CREATE_PRICE } from "../utils/mutations";
 import { QUERY_MATCHUPS } from "../utils/queries";
 
 const Login = () => {
@@ -12,11 +15,41 @@ const Login = () => {
 
   const matchup = data?.matchups || [];
 
-  const [createVote, { error }] = useMutation(CREATE_VOTE);
+  const [createDeparture, { error }] = useMutation(CREATE_DEPARTURE);
+  const [createDestination, { error }] = useMutation(CREATE_DESTINATION);
+  const [createDuration, { error }] = useMutation(CREATE_DURATION);
+  const [createPrice, { error }] = useMutation(CREATE_PRICE);
 
   const handleVote = async (techNum) => {
     try {
-      await createVote({
+      await createDeparture({
+        variables: { _id: id, techNum: techNum },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const handleVote = async (techNum) => {
+    try {
+      await createDestination({
+        variables: { _id: id, techNum: techNum },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const handleVote = async (techNum) => {
+    try {
+      await createDuration({
+        variables: { _id: id, techNum: techNum },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const handleVote = async (techNum) => {
+    try {
+      await createPrice({
         variables: { _id: id, techNum: techNum },
       });
     } catch (err) {
@@ -54,43 +87,47 @@ const Login = () => {
         <div className="login-contentbox">
           <form className="login-form">
             <h2>Login</h2>
-            <label for="login-email">Email Address: </label>
+            <label htmlFor="login-email">Email Address: </label>
             <input
               type="text"
               className="login-email"
               placeholder="Email Address"
             ></input>
             <br></br>
-            <label for="login-email">Password: </label>
+            <label htmlFor="login-email">Password: </label>
             <input
               type="text"
               className="login-pass"
               placeholder="Password"
             ></input>
+            <br></br>
+            <input  type="submit" value="Login"></input>
           </form>
 
           <form className="signup-form">
             <h2>Sign Up</h2>
-            <label for="signup-email">Full Name: </label>
+            <label htmlFor="signup-email">Full Name: </label>
             <input
               type="text"
               className="signup-name"
               placeholder="First Last"
             ></input>
             <br></br>
-            <label for="signup-email">Email Address: </label>
+            <label htmlFor="signup-email">Email Address: </label>
             <input
               type="text"
               className="signup-email"
               placeholder="Email Address"
             ></input>
             <br></br>
-            <label for="signup-email">Password: </label>
+            <label htmlFor="signup-email">Password: </label>
             <input
               type="text"
               className="signup-pass"
               placeholder="Password"
             ></input>
+            <br></br>
+            <input  type="submit" value="Sign Up"></input>
           </form>
         </div>
 
