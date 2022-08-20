38cf1101874e099cd9cde116bcc6931e182b8479
bcheung827 <83896907+bcheung827@users.noreply.github.com>
2022-08-20 11:01:14 -0400

    adjusting query code

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,17 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_MATCHUPS = gql`
+  query matchups($_id: String) {
+    matchups(_id: $_id) {
+      _id
+      tech1
+      tech2
+      tech1_votes
+      tech2_votes
+    }
+  }
+`;
+
 export const QUERY_USERS = gql`
 query users {
   users {
@@ -10,23 +22,23 @@ query users {
 }
 `;
 
-export const QUERY_DEPARTURE = gql`
-query departure {
+// export const QUERY_DEPARTURE = gql`
+// query departure {
 
-}
-`;
-export const QUERY_DESTINATION = gql`
-query destination {
+// }
+// `;
+// export const QUERY_DESTINATION = gql`
+// query destination {
 
-}
-`;
-export const QUERY_PRICE = gql`
-query price {
+// }
+// `;
+// export const QUERY_PRICE = gql`
+// query price {
 
-}
-`;
-export const QUERY_DURATION = gql`
-query duration {
+// }
+// `;
+// export const QUERY_DURATION = gql`
+// query duration {
 
-}
-`;
+// }
+// `;
