58d9f1ca6240415b9d0d2006c4efc8e529184cb4
Sameer Patel <spatel7793@gmail.com>
2022-08-20 10:59:18 -0400

    last query file never saved properly...fixed here

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -12,21 +12,34 @@ query users {
 
 export const QUERY_DEPARTURE = gql`
 query departure {
-
+  departure {
+    city
+    airport
+  }
 }
 `;
 export const QUERY_DESTINATION = gql`
 query destination {
-
+    destination {
+      city
+      airport
+    }
 }
 `;
+
 export const QUERY_PRICE = gql`
 query price {
+  price {
+    price
+  }
 
 }
 `;
 export const QUERY_DURATION = gql`
 query duration {
+  duration {
+    duration
+  }
 
 }
 `;
