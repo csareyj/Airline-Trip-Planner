6b35a3a658ae0880dd99b5c8f149a64cdbd06ad6
Sameer Patel <spatel7793@gmail.com>
2022-08-20 12:28:55 -0400

    added to seed file so we have some data to work with

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,25 +1,17 @@
 const db = require('../config/connection');
-const { User, Departure, Destination, Duration, Price } = require('../models');
+const { User, Flights } = require('../models');
 
 const userData = require('./userData.json');
-const depData = require('./depData.json');
-const destData = require('./destData.json');
-const durData = require('./durData.json');
-const priceData = require('./priceData.json');
+const flightinfoData = require('./flightinfoData.json')
 
 db.once('open', async () => {
   
   await User.deleteMany({});
-  await Departure.deleteMany({});
-  await Destination.deleteMany({});
-  await Price.deleteMany({});
-  await Duration.deleteMany({});
+  await Flights.deleteMany({});
 
   const users = await User.insertMany(userData);
-  const departure = await Departure.insertMany(depData);
-  const destination = await Destination.insertMany(destData);
-  const price = await Price.insertMany(priceData);
-  const duration = await Duration.insertMany(durData);
+  const flights = await Flight.insertMany(depData);
+  
 
   console.log('All data seeded!');
   process.exit(0);
