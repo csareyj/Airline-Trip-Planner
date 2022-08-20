3663196a0d3d9c7d87998acf90cd28831b600294
Curtis <csareyj@gmail.com>
2022-08-20 10:04:37 -0700

    Login Mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -47,3 +47,15 @@ mutation createUser($name: String!, $email: String!, $password: String!) {
   }
 }
 `;
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user{
+      _id
+      name
+      email
+    }
+  }
+}
+`;
