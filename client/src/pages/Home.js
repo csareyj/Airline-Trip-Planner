bd0f4f37e0761af903972d76bc714281fcd96d2a
DaveDw <93271098+DaveDw@users.noreply.github.com>
2022-08-23 15:31:40 -0400

    added filter functionality

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 // import { QUERY_USERS } from "../utils/queries";
 import { QUERY_FLIGHTS } from "../utils/queries";
-import { CREATE_FLIGHTS } from "../utils/mutations";
+// import { CREATE_FLIGHTS } from "../utils/mutations";
 
 const Home = () => {
   const { data } = useQuery(QUERY_FLIGHTS, {
@@ -15,17 +15,20 @@ const Home = () => {
 
   const flightList = data?.flights || [];
 
-  const [flightsData, setFlightData] = useState(flightList);
-  const [searchOption, setSearchOption] = useState("name");
+  // const [flightsData, setFlightData] = useState(flightList);
+  // const [searchOption, setSearchOption] = useState("name");
+  const [query, setQuery] = useState("");
+  const [query2, setQuery2] = useState("");
 
-  const handleSearchChange = (event) => {
-    setFlightData(flightList.filter(character => character[searchOption].toString().toLowerCase().includes(event.target.value.toLowerCase)))
-  }
+  // const handleSearchChange = (event) => {
+  //   setFlightData(flightList.filter(character => character[searchOption].toString().toLowerCase().includes(event.target.value.toLowerCase)))
+  // }
 
-  const handleSearchOptionChange = (event) => {
-    setSearchOption(event.target.value);
-  }
+  // const handleSearchOptionChange = (event) => {
+  //   setSearchOption(event.target.value);
+  // }
 
+  console.log(flightList.filter(flight=>flight.departure.toLowerCase().includes(query.toLowerCase())));
   return (
     <div className="App">
       <header className="App-header">
@@ -58,21 +61,19 @@ const Home = () => {
 
       <h2>Where To?</h2>
 
-      <form className="App-search" onChange={handleSearchOptionChange}>
-        <input type="input" placeholder="From..." onChange={handleSearchChange}></input>
-        <h1></h1>
-        <input type="input" placeholder="To..." onChange={handleSearchChange}></input>
-        <div>
-          <button className="App-search-btn">Search</button>
-        </div>
+      <form className="App-search">
+        <input type="text" placeholder="From..." onChange={e=>setQuery(e.target.value)} className="search-bar"></input>
+        <h1>🠒</h1>
+        <input type="text" placeholder="To..." onChange={e=>setQuery2(e.target.value)}></input>
       </form>
 
       <h2 onClick={testFunction}>Results:</h2>
 
-      <section className="App-results">
+      <ul className="App-results">
         {/* here we generate the result cards, I'll leave a template card but I don't yet know how to generate them off of the database info */}
-        {flightList.map(flight => {
+        {flightList.filter(flight=>flight.departure.toLowerCase().includes(query.toLowerCase())).filter(flight=>flight.destination.toLowerCase().includes(query2.toLowerCase())).map(flight => {
           return (
+            <li>
             <div className="App-card">
             <h3>FLIGHT {flight.flightNumber}</h3>
             <div className="Card-ori-dest">
@@ -86,9 +87,10 @@ const Home = () => {
             <p>{flight.price}</p>
             <button>Book Flight</button>
           </div>
+          </li>
           )
         })}
-      </section>
+      </ul>
 
       <footer>
         <h3>
